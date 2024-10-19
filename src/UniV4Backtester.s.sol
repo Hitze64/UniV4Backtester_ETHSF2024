907e49f463366aadf360af6e2bee461e9fc7d4f3
tommyzhao451 <tommy@aperture.finance>
2024-10-19 10:38:22 -0700

    added TODOs

--- src/UniV4Backtester.s.sol
+++ src/UniV4Backtester.s.sol
@@ -1,11 +1,22 @@
 // SPDX-License-Identifier: MIT
+// $ forge script src/UniV4Backtester.s.sol
 pragma solidity >=0.8.0;
 
 import {PoolEvent} from "./SUniV4Backtester.sol";
+import {PoolKey} from "lib/v4-core/src/types/PoolKey.sol";
+import {Script, console} from "forge-std/Script.sol";
 
-contract UniV4Backtester {
+contract UniV4Backtester is Script {
     // Returns mint/swap/burn events from UniV3 pool since pool creation until endDate.
-    function getPoolEvents(/*add poolKey,*/uint128 endDate) external view returns (PoolEvent[] memory poolEvents) {
-        // TODO
+    function getPoolEvents(
+        PoolKey calldata poolKey,
+        uint128 endDate
+    ) external view returns (PoolEvent[] memory poolEvents) {
+        // TODO @gnarlycow
+    }
+
+    function run() public {
+        // TODO @tommyzhao451
+        console.log("Hello World!");
     }
 }
