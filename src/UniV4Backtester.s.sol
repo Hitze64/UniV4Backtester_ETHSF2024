24f36c457fa4e0670c354352542c7a024e5e06c5
tommyzhao451 <tommy@aperture.finance>
2024-10-19 17:05:55 -0700

    stash

--- src/UniV4Backtester.s.sol
+++ src/UniV4Backtester.s.sol
@@ -2,9 +2,11 @@
 // $ forge script UniV4Backtester --fork-url https://unichain-sepolia.g.alchemy.com/v2/0123456789ABCDEFGHIJKLMNOPQRSTUV --fork-block-number 2523275
 pragma solidity >=0.8.0;
 
+import "./ICreate2Deployer.sol";
 import "lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 
 // import {AutoCompound} from "./hooks/AutoCompound.sol";
+import {deployCodeTo} from "forge-std/src/StdCheats.sol";
 import {Script, console} from "forge-std/Script.sol";
 import {NoopHook} from "./hooks/NoopHook.sol";
 import {PoolEvent, PoolEventType} from "./SUniV4Backtester.sol";
@@ -16,6 +18,7 @@ import {Currency} from "lib/v4-periphery/lib/v4-core/src/types/Currency.sol";
 import {PoolKey} from "lib/v4-periphery/lib/v4-core/src/types/PoolKey.sol";
 import {IPositionManager} from "lib/v4-periphery/src/interfaces/IPositionManager.sol";
 import {PoolManager} from "lib/v4-periphery/lib/v4-core/src/PoolManager.sol";
+import {HookMiner} from "lib/v4-template/test/utils/HookMiner.sol";
 
 contract UniV4Backtester is Script {
     // Returns mint/swap/burn events from UniV3 pool since pool creation until endDate.
@@ -35,21 +38,33 @@ contract UniV4Backtester is Script {
     }
 
     function run() public {
+        address unichainPoolManagerAddress = 0xC81462Fec8B23319F288047f8A03A57682a35C1A; // https://docs.uniswap.org/contracts/v4/deployments
+        address unichainCreate2Deployer = 0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2; // https://github.com/pcaversaccio/create2deployer
+        address hookAddress = 0x0000000000000000000000000000000000000000; // Set as 0 to mine a new one
+
         // Deploy token1
+        console.log("Before deploying Token1");
         Token1 token1 = new Token1();
-        console.log("Deployed Token1@", address(token1));
-        console.log(token1.balanceOf(address(this)));
+        console.log("After deploying Token1@", address(token1), ", token1.balanceOf(address(this))=", token1.balanceOf(address(this)));
 
         // Pool Manager
         PoolManager poolManager = PoolManager(0xC81462Fec8B23319F288047f8A03A57682a35C1A);
 
         // Hook info
-        console.log("before deploying noop hook");
-        uint160 flags = 0;
-        bytes memory constructorArgs = abi.encode(0xC81462Fec8B23319F288047f8A03A57682a35C1A);
-        NoopHook noopHook = new NoopHook(poolManager);
-        (address hookAddress, bytes32 salt) = HookMiner.find(CREATE2_DEPLOYER, flags, type(PointsHook).creationCode, constructorArgs);
-        console.log("Deployed noopHook@", address(noopHook));
+        if (hookAddress == address(0)) {
+            uint160 flags = 0;
+            bytes memory constructorArgs = abi.encode(unichainPoolManagerAddress);
+            (address minedHookAddress, bytes32 salt) = HookMiner.find(unichainCreate2Deployer, flags, type(NoopHook).creationCode, constructorArgs);
+            hookAddress = minedHookAddress;
+            console.log("Finished Mining Hook Address, hookAddress=", hookAddress);
+        }
+        Create2Deployer create2deployer = Create2Deployer(unichainCreate2Deployer);
+        create2deployer.deploy(0, automanSalt, initCode);
+        deployCodeTo("NoopHook.sol", abi.encode(unichainPoolManagerAddress), hookAddress);
+        console.log("After deploying Hook@", address(hookAddress));
+        NoopHook noopHook = NoopHook(hookAddress);
+
+        // console.log("Deployed noopHook@", address(noopHook));
 
         // Pool info
         // https://sepolia.uniscan.xyz/address/0xC81462Fec8B23319F288047f8A03A57682a35C1A
@@ -57,8 +72,8 @@ contract UniV4Backtester is Script {
         Currency currency1 = Currency.wrap(address(token1)); // deployed usdc on sepolia.unichain
         uint24 fee = 3000;
         int24 tickSpacing = 60;
-        PoolKey memory poolKey = PoolKey(currency0, currency1, fee, tickSpacing, IHooks(address(noopHook)));
-        poolManager.initialize(poolKey, 79228162514264337593543950336);
+        // PoolKey memory poolKey = PoolKey(currency0, currency1, fee, tickSpacing, IHooks(address(noopHook)));
+        // poolManager.initialize(poolKey, 79228162514264337593543950336);
 
         // // Position info
         // // https://sepolia.uniscan.xyz/address/0xB433cB9BcDF4CfCC5cAB7D34f90d1a7deEfD27b9
